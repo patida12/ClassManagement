c49b090048a7f09c9e649bacc1a547d0c25d3d66
Patida <patida@patida-VivoBook-15-ASUS-Laptop-X507UF>
2020-09-16 01:20:20 +0700

    create index.php

--- src/index.php
+++ src/index.php
@@ -1,3 +1,40 @@
-<?php
-echo "Hello world"
-?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Class Management</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+   
+    <link rel="stylesheet" type="text/css" href="/css/styles.css"/>
+   
+</head>
+<body>
+<header>
+  <h3><i class="fa fa-graduation-cap">ClassManagement</i></h3>
+</header>
+
+<section>
+<div class="sidebar">
+    <a class="active" href="#home"><i class="fa fa-home"> Home</i></a>
+    <a href="#assignment"><i class="fa fa-book"> Assignment</i></a>
+    <a href="#teachers"><i class="fa fa-user"> Teachers</i></a>
+    <a href="#students"><i class="fa fa-users"> Students</i></a>
+    <a href="#quizs"><i class="fa fa-gamepad"> Quizs</i></a>
+    <a href="#profile"><i class="fa fa-user-circle"> Profile</i></a>
+  </div>
+  
+  <div class="content">
+    <h1>London</h1>
+    <p>London is the capital city of England. It is the most populous city in the  United Kingdom, with a metropolitan area of over 13 million inhabitants.</p>
+    <p>Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium.</p>
+</div>
+</section>
+
+</body>
+</html>
